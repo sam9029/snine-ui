d5e26edcf851f4aefefed7cdc42b6e3e898e211f
dongxiyao <dongxiyao@bjjiayin.cn>
2023-08-07 12:19:34 +0800

    init 组件库初始化

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   presets: ["@vue/cli-plugin-babel/preset"],
+  // presets: ["@vue/cli-plugin-babel/preset", "@vue/app"],
+  // 允许两种编码引入方式共存,也就是common规范与es6规范的共存处理
+  sourceType: "unambiguous",
 };
--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,5 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
-  </div>
+  <div id="app">hihi</div>
 </template>
 
 <style lang="scss">
