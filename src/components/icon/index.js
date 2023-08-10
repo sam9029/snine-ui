59c249ca2a0bbdb3ad4086fef7054808678182ad
dongxiyao <dongxiyao@bjjiayin.cn>
2023-08-10 11:55:07 +0800

    add:icon组件&引入elui的icon资源

---
+++ src/components/icon/index.js
@@ -0,0 +1,8 @@
+import SnIcon from "./main/icon.vue";
+
+/* istanbul ignore next */
+SnIcon.install = function (Vue) {
+  Vue.component(SnIcon.name, SnIcon);
+};
+
+export default SnIcon;
---
+++ src/components/icon/main/icon.vue
@@ -0,0 +1,16 @@
+<template>
+  <i :class="'sn-icon-' + name"></i>
+</template>
+
+<script>
+export default {
+  name: "SnIcon",
+  props: {
+    name: String,
+  },
+};
+</script>
+
+<!-- 
+  icon类型
+ -->
