e24ecdd74dad09423bb9f579d18eb73fee15623e
dongxiyao <dongxiyao@bjjiayin.cn>
2023-08-11 17:20:59 +0800

    mod:base-配置组件引用换回本地组件引用

--- examples/main.js
+++ examples/main.js
@@ -3,16 +3,16 @@ import App from "./App.vue";
 // import router from "./router";
 
 //#region 打包前本地组件文件直接引入
-// // 引入组件 (../packages/index.js 中 做了注册组件处理&引入样式处理)
-// import SnineUI from "../packages/index.js";
-// // 全局挂载
-// Vue.use(SnineUI);
+// 引入组件 (../packages/index.js 中 做了注册组件处理&引入样式处理)
+import SnineUI from "../packages/index.js";
+// 全局挂载
+Vue.use(SnineUI);
 //#endregion
 
 //#region 打包后引入
-import SnineUI from "../lib/snineUI.umd.min.js";
-import "../lib/snineUI.css";
-Vue.use(SnineUI);
+// import SnineUI from "../lib/snineUI.umd.min.js";
+// import "../lib/snineUI.css";
+// Vue.use(SnineUI);
 //#endregion
 
 Vue.config.productionTip = false;
