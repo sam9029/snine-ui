dd08148efbfa5449cb5804bc4e3887d589a17561
dongxiyao <dongxiyao@bjjiayin.cn>
2023-08-15 15:25:29 +0800

    mod:base standardized App.vue components naming

--- examples/App.vue
+++ examples/App.vue
@@ -2,29 +2,32 @@
   <div id="app">
     <div class="left"></div>
     <div class="right">
-      <SnButton />
-      <!-- <SnIcon /> -->
-      <!-- <SnUpload /> -->
-      <SnTag />
-      <SnSelect />
+      <!-- <SnButtonComp /> -->
+      <!-- <SnIconComp /> -->
+      <!-- <SnUploadComp /> -->
+      <!-- <SnTagComp /> -->
+      <!-- <SnSelectComp /> -->
+      <SnInputComp />
     </div>
   </div>
 </template>
 
 <script>
-import SnButton from "./demo/SnButton.vue";
-// import SnIcon from "./demo/SnIcon.vue";
-// import SnUpload from "./demo/SnUpload.vue";
-import SnTag from "./demo/SnTag.vue";
-import SnSelect from "./demo/SnSelect.vue";
+// import SnButtonComp from "./demo/SnButton.vue";
+// import SnIconComp from "./demo/SnIcon.vue";
+// import SnUploadComp from "./demo/SnUpload.vue";
+// import SnTagComp from "./demo/SnTag.vue";
+// import SnSelectComp from "./demo/SnSelect.vue";
+import SnInputComp from "./demo/SnInput.vue";
 export default {
   name: "APP",
   components: {
-    SnButton,
-    // SnIcon,
-    // SnUpload,
-    SnTag,
-    SnSelect,
+    // SnButtonComp,
+    // SnIconComp,
+    // SnUploadComp,
+    // SnTagComp,
+    // SnSelectComp,
+    SnInputComp,
   },
 };
 </script>
